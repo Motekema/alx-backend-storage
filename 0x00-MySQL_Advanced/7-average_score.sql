e6f978202cdd3e4ca6b7e6ad1c72bb48eedc12cf
T.K Mahlanya <91951223+Motekema@users.noreply.github.com>
2024-04-25 10:49:50 +0200

    My commit

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,28 +1,23 @@
-DELIMITER //
-
-CREATE PROCEDURE ComputeAverageScoreForUser(
-    IN p_user_id INT
-)
+-- Create a stored procedure Computer Average Score For the User that
+-- computer and store the average score for a student.
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+DELIMITER $$
+CREATE PROCEDURE ComputeAverageScoreForUser (user_id INT)
 BEGIN
-    DECLARE v_total_score INT;
-    DECLARE v_total_projects INT;
-    DECLARE v_average_score FLOAT;
+    DECLARE total_score INT DEFAULT 0;
+    DECLARE projects_count INT DEFAULT 0;
 
-    -- Calculate total score for the user
-    SELECT SUM(score) INTO v_total_score FROM corrections WHERE user_id = p_user_id;
+    SELECT SUM(score)
+        INTO total_score
+        FROM corrections
+        WHERE corrections.user_id = user_id;
+    SELECT COUNT(*)
+        INTO projects_count
+        FROM corrections
+        WHERE corrections.user_id = user_id;
 
-    -- Calculate total number of projects for the user
-    SELECT COUNT(DISTINCT project_id) INTO v_total_projects FROM corrections WHERE user_id = p_user_id;
-
-    -- Calculate average score
-    IF v_total_projects > 0 THEN
-        SET v_average_score = v_total_score / v_total_projects;
-    ELSE
-        SET v_average_score = 0;
-    END IF;
-
-    -- Update average score for the user
-    UPDATE users SET average_score = v_average_score WHERE id = p_user_id;
-END//
-
+    UPDATE users
+        SET users.average_score = IF(projects_count = 0, 0, total_score / projects_count)
+        WHERE users.id = user_id;
+END $$
 DELIMITER ;
