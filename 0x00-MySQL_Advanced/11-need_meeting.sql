2bc65a73635f8a8e9a5565c57e715a77e759653e
Motekema <motekema@gmail.com>
2024-04-25 02:01:30 -0700

    My commit

---
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -0,0 +1,5 @@
+-- Create a view need_meeting that lists all students needing a meeting
+CREATE VIEW need_meeting AS
+SELECT name
+FROM students
+WHERE (score < 80 OR last_meeting IS NULL OR last_meeting < DATE_SUB(CURDATE(), INTERVAL 1 MONTH));
