8821b5d0112e4b4fdf544015db5c0b846887dbba
T.K Mahlanya <91951223+Motekema@users.noreply.github.com>
2024-04-30 13:29:48 +0200

    Update 12-log_stats.py

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -5,35 +5,23 @@ Log Stats
 
 from pymongo import MongoClient
 
-
-def count_logs(collection):
-    """Counts the number of documents in a collection"""
-    return collection.count_documents({})
-
-def count_methods(collection):
-    """Counts the number of documents for each HTTP method"""
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    counts = {}
-    for method in methods:
-        counts[method] = collection.count_documents({"method": method})
-    return counts
-
-def count_status_check(collection):
-    """Counts the number of status check requests"""
-    return collection.count_documents({"method": "GET", "path": "/status"})
-
-def print_stats(log_count, method_counts, status_check_count):
-    """Prints the statistics"""
-    print(f"{log_count} logs")
-    print("Methods:")
-    for method, count in method_counts.items():
-        print(f"\tmethod {method}: {count}")
-    print(f"{status_check_count} status check")
-
 if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
-    logs_collection = client.logs.nginx
-    log_count = count_logs(logs_collection)
-    method_counts = count_methods(logs_collection)
-    status_check_count = count_status_check(logs_collection)
-    print_stats(log_count, method_counts, status_check_count)
+    nginx_collection = client.logs.nginx
+    number = nginx_collection.count()
+    number_get = nginx_collection.find({"method": "GET"}).count()
+    number_post = nginx_collection.find({"method": "POST"}).count()
+    number_put = nginx_collection.find({"method": "PUT"}).count()
+    number_patch = nginx_collection.find({"method": "PATCH"}).count()
+    number_delete = nginx_collection.find({"method": "DELETE"}).count()
+    number_status = nginx_collection.find(
+        {"method": "GET", "path": "/status"}).count()
+
+    print("{} logs".format(number))
+    print("Methods:")
+    print("\tmethod GET: {}".format(number_get))
+    print("\tmethod POST: {}".format(number_post))
+    print("\tmethod PUT: {}".format(number_put))
+    print("\tmethod PATCH: {}".format(number_patch))
+    print("\tmethod DELETE: {}".format(number_delete))
+    print("{} status check".format(number_status))
