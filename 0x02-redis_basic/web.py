093999fd6299abef1f47b85b43864f6a490ed967
T.K Mahlanya <91951223+Motekema@users.noreply.github.com>
2024-05-01 11:41:39 +0200

    Update web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -2,43 +2,32 @@
 """
 Web Cache Module
 """
-import requests
-import redis
-import time
 from functools import wraps
+import redis
+import requests
 from typing import Callable
 
-def track_access(func: Callable) -> Callable:
-    """
-    Decorator to track the number of times a URL is accessed
-    """
-    @wraps(func)
-    def wrapper(url: str) -> str:
-        """
-        Wrapper function to track URL access and cache the result
-        """
-        # Increment access count for the URL
-        redis_client = redis.Redis()
-        redis_client.incr(f"count:{url}")
-        
-        # Cache the result with an expiration time of 10 seconds
-        cached_content = redis_client.get(url)
-        if cached_content:
-            return cached_content.decode('utf-8')
-        else:
-            content = func(url)
-            redis_client.setex(url, 10, content)
-            return content
+redis_ = redis.Redis()
+
+
+def count_requests(method: Callable) -> Callable:
+    """ Decorator for counting """
+    @wraps(method)
+    def wrapper(url):  # sourcery skip: use-named-expression
+        """ Wrapper for decorator """
+        redis_.incr(f"count:{url}")
+        cached_html = redis_.get(f"cached:{url}")
+        if cached_html:
+            return cached_html.decode('utf-8')
+        html = method(url)
+        redis_.setex(f"cached:{url}", 10, html)
+        return html
+
     return wrapper
 
-@track_access
-def get_page(url: str) -> str:
-    """
-    Retrieve HTML content of a URL
-    """
-    response = requests.get(url)
-    return response.text
 
-if __name__ == "__main__":
-    url = "http://slowwly.robertomurray.co.uk/delay/1000/url/http://www.google.co.uk"
-    print(get_page(url))
+@count_requests
+def get_page(url: str) -> str:
+    """ Obtain the HTML contents of a  URL """
+    req = requests.get(url)
+    return req.text
